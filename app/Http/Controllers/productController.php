0e8a8937aaf31eb1ac5ca33d996e38bdff3f8594
VyachrslavR <detalline@yandex.ru>
2021-11-09 17:14:36 +0300

    submitting a form

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -13,32 +13,50 @@ class productController extends Controller
         $info = json_decode($info, true);
 
         if ($info['success']) {
+            //print_r($info);
+            //$ides=$info['products']['offers']['id'];
             $sus = 'получен продукт';
-           
-            $url='https://superposuda.retailcrm.ru/api/v5/orders/create';   
 
-             $data = array('contragent' => 'individual', 
-             'order'=>array('items' => 'ddd', ''=>'','offer'=>'0', 'id'=>'23426'),
-             'apiKey'=>'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb');
-             $data = http_build_query($data);
+            $url = 'https://superposuda.retailcrm.ru/api/v5/orders/create';
+            if(isset($_GET)){
+                $order_n=$_GET['order-n']; $fio=$_GET['fio']; $article=$_GET['article'];
+            $brand=$_GET['brand']; $comment=$_GET['comment'];
+            }
+            $data = array(
+                'contragent' => 'individual',
+                'order' => array('items' => $order_n, '' => '', 'offer' => '0', 'id' => '23426',
+                'statusComment'=>$comment, 'status' => 'trouble',
+            ),
+                'apiKey' => 'QlnRWTTWw9lv3kjxy1A8byjUmBQedYqb'
+            );
+            $data = http_build_query($data);
 
             $context_options = array(
                 'http' => array(
                     'method' => 'POST',
-                    'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                    'header' => "Content-type: application/x-www-form-urlencoded\r\n" .
+                        "Content-Length: " . strlen($data) . "\r\n",
                     'content' => $data
                 )
             );
 
-             $context = stream_context_create($context_options);
-             $fp = file_get_contents($url, false, $context);
-             $page = json_decode($fp, true);
-            //$page=array('success'=>'1', 'order'=>array('id'=>'45555', 'orderType'=>'fizik'));
-            print_r($page);
-            if (1) {
-                $idorder = $page['order']['id'];
-                $orderType = $page['order']['orderType'];
-                $sus .= ':- заказ создан' . ':' . $_GET['order-f'] . ':' . $_GET['fio'] . ':' . $_GET['article'] .
+            $context = stream_context_create($context_options);
+            //$fp = file_get_contents($url, false, $context);
+            //$page = json_decode($fp, true);
+            $page=array('success'=>'1', 'order'=>array('id'=>'45555', 'orderType'=>'fizik'));
+            //print_r($page);
+            if (isset($page['success'])) {
+                  $idorder = $page['order']['id'];
+                  $orderType = $page['order']['orderType'];
+                  $clientfio = $page['order']['orderType'];
+                  $order_number = $page['order']['orderType'];
+                  $order_status = $page['order']['orderType'];
+                  $shop = $page['order']['orderType'];
+                  $registration_method = $page['order']['orderType'];
+                  $comments = $page['order']['orderType'];
+                  $id_product = $page['order']['orderType'];
+                  $zaka_item_number = $page['order']['orderType'];
+                $sus .= ':- заказ создан' . ':' . $_GET['order-n'] . ':' . $_GET['fio'] . ':' . $_GET['article'] .
                     ':' . $_GET['brand'] . ':' . $_GET['comment'];
             } else {
                 $sus .= ':- сервер не создал заказ';
@@ -46,6 +64,11 @@ class productController extends Controller
         } else {
             $sus = 'сервер не дал продукт';
         }
-        return view('creating-an-order', ['sus' => $sus, 'page' => $idorder, 'orderType' => $orderType]);
+        return view('creating-an-order', [
+            'sus' => $sus, 'id_order' => $idorder, 'orderType' => $orderType, 'clientfio' => $clientfio, 
+            'order_number' => $order_number, 'order_status' => $order_status, 'shop' => $shop, 
+            'registration_method' => $registration_method,
+            'comments' => $comments, 'id_product' => $id_product, 'zaka_item_number' => $zaka_item_number
+        ]);
     }
 }
--- resources/views/creating-an-order.blade.php
+++ resources/views/creating-an-order.blade.php
@@ -6,13 +6,13 @@
 <div class='input-group flex-nowrap d-flex justify-content-center'>
   <form action="{{route('get-p')}}" id='form-creating' method="get">
     <label for="order">НОМЕР ЗАКАЗА</label>
-    <input readonly class="form-control" id='order-f' name='order-f'  value='27041981'>
+    <input readonly class="form-control" id='order-n' name='order-n'  value='27041981'>
     <label for="fio">Ваше Фамилия Имя Отчество:</label>
     <input class="form-control" id='fio' name='fio' type="text"  placeholder='тут пишем ФИО'>
     <label for="article">Артикул</label>
-    <input class="form-control" id='article' name='article' type="text" placeholder='сюда пишем артикул'>
+    <input class="form-control" id='article' name='article' type="text" placeholder='артикул:02120623-0767'>
     <label for="brand">Бренд</label>
-    <input class="form-control" id='brand' name='brand' type="text" placeholder='сюда пишем бренд'>
+    <input class="form-control" id='brand' name='brand' type="text" placeholder='сюда пишем бренд:Leander'>
     <div class="form-group">
       <label for="exampleFormControlTextarea1">Коментарий к заказу</label>
       <textarea class="form-control" id="comment" name='comment' rows="3" placeholder='github'></textarea>
@@ -29,13 +29,13 @@
 
 </div>
 @endif
-  @if (isset($page))
+  @if (isset($id_order))
   
   <div class="alert alert-success" role="alert">
-    <P>Номер заказ: {{ $page }}</P>
-    <p>Тип заказа: {{ $orderType }}</p>
-    
-  
+    <P>Клиент фио: {{$clientfio}}'  'Номер заказ: {{ $order_number }}'  'Тип заказа: {{ $orderType }}'  'Статус заказа: {{ $order_status }}</p>
+    <P>Магазин: {{ $shop }}'  'Способ оформления: {{ $registration_method }}'  'Комментарий: {{ $comments }}</p>
+    <P>id Товара {{ $id_product }}'  'id заказа: {{ $id_order }}'  'Артикул товара: {{ $zaka_item_number }}</p>
+   
   </div>
 
   @endif
